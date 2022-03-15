5c42eceae58c77997248bc777ada63f2b9b2452f
Tudor Gălățan <tudorgalatan@gmail.com>
2022-03-15 15:48:45 +0200

    Updated Project
    I have updated the project.

---
+++ Project/Example_29.sql
@@ -0,0 +1,12 @@
+/*
+    Example 29 - Clean the Workspace
+*/
+
+
+DROP PROCEDURE calculate_sum;
+DROP PROCEDURE increment_date;
+DROP PROCEDURE increment_number;
+DROP PROCEDURE print_message;
+DROP PROCEDURE print_name;
+DROP PROCEDURE to_power;
+DROP PROCEDURE to_power_modified;
